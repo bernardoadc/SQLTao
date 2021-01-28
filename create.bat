fd8ace785877d27fbb4db0c020cec7b97e3d84e3
Bernardo A. Dal Corno (c082647) <c082647@mail.caixa>
2021-01-28 15:13:30 -0300

    Initial

---
+++ create.bat
@@ -0,0 +1,1 @@
+.\node_modules\SchemaZen\SchemaZen.exe create --server %1 --database %2 --scriptDir SQLExportedDB/create --overwrite
---
+++ script.bat
@@ -0,0 +1,1 @@
+.\node_modules\SchemaZen\SchemaZen.exe script --server %1 --database %2 --scriptDir SQLExportedDB --dataTables=%3
